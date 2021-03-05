e8e892d8911f44fdaf7c28e7546e1ea57c949c5d
Shany Danieli <shany.danieli@yale.edu>
2021-03-05 15:28:24 +0200

    add setup

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup (
+    name = 'DFDetect',
+    version='0.0.1',
+    author='Tim Miller, Shany Danieli',
+    description='Source Detection Tools for the Dragonfly Wide Field Survey',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7'
+    )
